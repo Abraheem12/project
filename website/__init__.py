2b0ca4a26312457e15d4f6f0e05b4717e5bf11ab
Abraheem12 <abraheem.zadron@ucdenver.edu>
2024-04-08 22:11:37 -0600

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from os import getenv
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY', 'default_secret_key')  # Use environment variable
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///default.db')  # Use environment variable
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    #so this models runs before the database is created run all of those functions in models before hand
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    return app
+
+
+
+#if the database is created or not then create it
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+

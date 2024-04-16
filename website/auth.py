2c33ad0fb85879816d49c74a032e0cbb2dbba78f
Robin Shrestha <robinshrestha089@gmail.com>
2024-04-15 22:03:48 -0600

    Refactor website models and views

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,36 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import UserMixin
 
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data["_id"])
+        self.email = user_data["email"]
+        self.password = user_data["password"]
+        self.first_name = user_data["first_name"]
+
+    @staticmethod
+    def get(user_id):
+        user_data = db.db.users.find_one({"_id": user_id})
+        if user_data:
+            return User(user_data)
+        else:
+            return None
+
+    @staticmethod
+    def create(email, password, first_name):
+        user_data = {
+            "email": email,
+            "password": generate_password_hash(password, method='pbkdf2:sha256'),
+            "first_name": first_name,
+        }
+        result = db.db.users.insert_one(user_data)
+        return User.get(result.inserted_id)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 auth = Blueprint('auth', __name__)
@@ -11,19 +38,20 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-         email = request.form.get('email')
-         password = request.form.get('password')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-         user = User.query.filter_by(email=email).first()
-         if user:
-              if check_password_hash(user.password, password):
-                   flash("Logged in", category='success')
-                   login_user(user, remember=True)
-                   return redirect(url_for('views.home'))
-              else:
-                   flash("incorect", category = 'error')
-         else:
-              flash("email does not exit", category = 'error')
+        user_data = db.db.users.find_one({"email": email})
+        if user_data:
+            user = User(user_data)
+            if user.check_password(password):
+                flash("Logged in", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Incorrect password", category='error')
+        else:
+            flash("Email does not exist", category='error')
     return render_template("login.html", user=current_user)
 
 @auth.route('logout')
@@ -40,7 +68,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
+        user = db.db.users.find_one({"email": email})
         if user:
             flash("Email already exists.", category='error')
         elif len(email) < 4:
@@ -52,14 +80,14 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='pbkdf2:sha256'))
-
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
+            new_user = {
+                "email": email,
+                "first_name": first_name,
+                "password": generate_password_hash(password1, method='pbkdf2:sha256')
+            }
+            db.db.users.insert_one(new_user)
+            login_user(User(new_user), remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
-
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, session, redirect, url_for
+from flask import Blueprint, render_template, request 
 from flask_login import login_required, current_user
 import openai
 import os
@@ -9,26 +9,21 @@ views = Blueprint('views', __name__)
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+
 def chatWithGPT(prompt):
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": prompt}]
-        )
-        return response.choices[0].message.content.strip()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
+    user_input = None
+    response = None
     if request.method == 'POST':
-        user_input = request.form['user_input']
+        user_input = request.form.get('user_input')
+        # Process the user_input with your chatbot here...
         response = chatWithGPT(user_input)
-        session['response'] = response
-        return redirect(url_for('views.home'))  # Redirect to the same page
-
-    user_name = current_user.first_name if current_user else "User"
-    response = session.pop('response', f'Hello {user_name}, how can I help you today?')
-    return render_template("home.html", user=current_user, response=response)
-
-
-
+    return render_template('home.html', user=current_user, user_input=user_input, response=response)

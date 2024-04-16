46599433f34a9bc9156001aebd8add6b079906cd
Robin Shrestha <57116623+rbnsth@users.noreply.github.com>
2024-04-15 23:24:25 -0600

    Update model.py

--- website/model.py
+++ website/model.py
@@ -1,1 +1,9 @@
+from . import db # this works because were in the same directory where the database was made if we were outside we would do something like from website import db
+from flask_login import UserMixin #Flask login is just a module that helps us log users in 
+from sqlalchemy.sql import func
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))

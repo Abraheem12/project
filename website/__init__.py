04ce8fa600647fda1e10dee570729c6239866b06
Robin Shrestha <57116623+rbnsth@users.noreply.github.com>
2024-04-15 23:26:14 -0600

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,36 +1,44 @@
 from flask import Flask
-from flask_pymongo import PyMongo
-from os import getenv
-from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 from flask_login import LoginManager
+from os import getenv
 
-db = PyMongo()
-login_manager = LoginManager()
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
-    load_dotenv()
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = getenv("SECRET_KEY")
-    app.config['MONGO_URI'] = getenv("MONGO_URI")
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY', 'default_secret_key')  # Use environment variable
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///default.db')  # Use environment variable
     db.init_app(app)
-    
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-    
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+    #so this models runs before the database is created run all of those functions in models before hand
+    from .models import User
+
     with app.app_context():
-        from .auth import User
-        db.db.users.create_index("email", unique=True)
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
-        from .auth import User
-        user_data = db.db.users.find_one({"_id": id})
-        return User(user_data) if user_data else None
-
+        return User.query.get(int(id))
     return app
+
+
+
+#if the database is created or not then create it
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+
